d537a575df1f38179b920cb6a187b599fd523dfd
Shing Rigard <johnny-luu@hotmail.com>
2018-11-07 15:49:29 +0100

    Fridge had a bug where it changed the amounts at a seemingly random pattern when you changed the amounts of several beers without leaving the activity.
    Bug has been fixed now.

--- app/src/main/java/ch/beerpro/presentation/profile/myfridge/OnMyFridgeItemInteractionListener.java
+++ app/src/main/java/ch/beerpro/presentation/profile/myfridge/OnMyFridgeItemInteractionListener.java
@@ -1,19 +1,19 @@
 package ch.beerpro.presentation.profile.myfridge;
 
 import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ImageView;
 import ch.beerpro.domain.models.Beer;
 
-public interface OnMyFridgeItemInteractionListener extends TextWatcher {
+public interface OnMyFridgeItemInteractionListener {
 
     void onMoreClickedListener(ImageView photo, Beer beer);
 
     void onFridgeBeerDeleteClickedListener(Beer beer);
 
-    void onIncrementClickedListener(Beer beer);
+    void onIncrementClickedListener(Beer beer, View view);
 
-    void onDecrementClickedListener(Beer beer);
+    void onDecrementClickedListener(Beer beer, View view);
 
     void afterTextChanged(Beer beer, Editable s);
 }
